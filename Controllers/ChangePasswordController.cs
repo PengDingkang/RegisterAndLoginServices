307f946051aadc97929bf6979419571df5d15f52
Nagisa_Cheese <kotori@tongji.edu.cn>
2020-12-12 20:31:27 +0800

    Add API: Change password
    添加新的 API: 修改密码

---
+++ Controllers/ChangePasswordController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Newtonsoft.Json.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace RegisterAndLoginServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChangePasswordController : ControllerBase
+    {
+        /// <summary>
+        ///     修改密码
+        /// </summary>
+        /// <param name="jsonStr">新密码和旧密码的 json</param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult ChangePassword ([FromBody] object jsonStr)
+        {
+            int id;
+            ChangePasswordModel model;
+            try
+            {
+                var auth = new JwtSecurityTokenHandler().ReadJwtToken(HttpContext.Request.Headers["authorization"].ToString().Split(' ')[1]);
+                id = Convert.ToInt32(auth.Claims.First(t => t.Type.Equals(ClaimTypes.NameIdentifier))?.Value);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { error = "Unauthorized API call" });
+            }
+
+            try
+            {
+                JObject jo = JObject.Parse(jsonStr.ToString());
+                model = jo.ToObject<ChangePasswordModel>();
+            }
+            catch
+            {
+                return BadRequest(new { error = "Invalid input" });
+            }
+
+            try
+            {
+                Services.ChangePassword.UserPasswordChange(id, model.oldPw, model.newPw);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { error = $"Password change Failed: {e.Message}" });
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ Services/ChangePassword.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using RegisterAndLoginServices.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginServices.Services
+{
+    public class ChangePassword
+    {
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="idi">读者 ID</param>
+        /// <param name="oldPw">原密码</param>
+        /// <param name="newPw">新密码</param>
+        public static void UserPasswordChange(int idi, string oldPw, string newPw)
+        {
+            if (!GlobalFunctions.IsValidPassword(newPw))
+            {
+                throw new Exception("Password Invalid");
+            }
+            try
+            {
+                GlobalFunctions.VerifyPassword(idi, oldPw);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            string salt = Guid.NewGuid().ToString();
+            string pwHash = GlobalFunctions.EncryptPassword(newPw, salt);
+            RegisterModel newInfo = new RegisterModel
+            {
+                id = idi,
+                passwordHash = pwHash,
+                salt = salt
+            };
+            var result = DbContext.DBstatic.Updateable(newInfo).UpdateColumns(it => new { it.salt, it.passwordHash }).ExecuteCommand();
+            try
+            {
+                GlobalFunctions.VerifyPassword(idi, newPw);
+            }
+            catch (Exception) {
+                throw new Exception("Failed Change");
+            }
+
+        }
+    }
+}
