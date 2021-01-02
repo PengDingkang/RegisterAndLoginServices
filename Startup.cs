f414b7c79b117817f49a19003b43f5ceb84ee10d
Nagisa_Cheese <kotori@tongji.edu.cn>
2021-01-02 17:00:59 +0800

    Fix authentication problems

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using RegisterAndLoginServices.Services;
+using System;
+using System.Text;
 
 namespace RegisterAndLoginServices
 {
@@ -19,6 +24,23 @@ namespace RegisterAndLoginServices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.FromSeconds(30),
+                        ValidateIssuerSigningKey = true,
+                        ValidAudiences = new[] { "user", "admin", "suadmin" },
+                        ValidIssuer = GlobalVars.domain,
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GlobalVars.secret))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +52,8 @@ namespace RegisterAndLoginServices
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
