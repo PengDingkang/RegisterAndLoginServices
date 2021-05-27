b7010bde33b9357aa651a0e8237cff497c38dbad
Nagisa_Cheese <kotori@tongji.edu.cn>
2021-05-28 01:01:41 +0800

    Test

---
+++ UnitTest/RegisterTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegisterAndLoginServices.Services;
+using RegisterAndLoginServices.Models;
+
+namespace UnitTest
+{
+    [TestClass]
+    class RegisterTest
+    {
+        [TestMethod]
+        public void Register_With_Right_Contact_And_Password()
+        {
+            // Arrange
+            RegisterModel reg = new()
+            {
+                contact = "13944556677",
+                password = "password"
+            };
+            bool succeed = true;
+
+            // Act
+            try
+            {
+                Register.AccepteRegister(reg);
+            }
+            catch
+            {
+                succeed = false;
+            }
+
+            // Assert
+            Assert.IsTrue(succeed);
+        }
+    }
+}
