e4c7e352217b86d29aa9ce057526560c102a3a94
Nagisa Cheese <kotori@tongji.edu.cn>
2021-05-27 21:33:17 +0800

    Init test

---
+++ UnitTest/LoginTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
